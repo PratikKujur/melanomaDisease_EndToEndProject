ebe23da03db0d809b2436ee75428b7841cedee30
PratikKujur <pratikkujur121@gmail.com>
2024-05-29 11:38:28 +0530

    prepared base model added

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -15,7 +15,7 @@ import base64
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """reads yaml file and returns
-
+                                
     Args:
         path_to_yaml (str): path like input
 
