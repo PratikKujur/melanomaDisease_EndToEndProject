7d3269c3b81e463aa2eed3d40be319a0f2248f5c
PratikKujur <pratikkujur121@gmail.com>
2024-06-06 15:33:26 +0530

    app.py added

---
+++ app.py
@@ -0,0 +1,92 @@
+from flask import Flask, request, jsonify, render_template
+import os
+from flask_cors import CORS, cross_origin
+from cnnClassifier.utils.common import decodeImage
+import numpy as np
+import tensorflow as tf
+#from tensorflow.keras.models import load_model
+#from tensorflow.keras.preprocessing import image
+import os
+
+
+
+class PredictionPipeline:
+    def __init__(self,filename):
+        self.filename =filename
+
+
+    
+    def predict(self):
+        # load model
+        model = tf.keras.models.load_model('artifacts/training/model.h5')
+
+        imagename = self.filename
+        test_image = tf.keras.preprocessing.image.load_img(imagename, target_size = (224,224))
+        test_image = tf.keras.preprocessing.image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = np.argmax(model.predict(test_image), axis=1)
+        print(result)
+
+        if result[0] == 1:
+            prediction = 'Malignant(Cancerous)'
+            return [{ "image" : prediction}]
+        else:
+            prediction = 'Benign(Non-Cancerous)'
+            return [{ "image" : prediction}]
+        
+
+
+
+
+
+
+
+
+
+
+
+
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
+
+app = Flask(__name__)
+CORS(app)
+
+
+class ClientApp:
+    def __init__(self):
+        self.filename = "inputImage.jpg"
+        self.classifier = PredictionPipeline(self.filename)
+
+
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    return render_template('index.html')
+
+
+
+
+@app.route("/train", methods=['GET','POST'])
+@cross_origin()
+def trainRoute():
+    #os.system("python main.py")
+    os.system("dvc repro")
+    return "Training done successfully!"
+
+
+
+@app.route("/predict", methods=['POST'])
+@cross_origin()
+def predictRoute():
+    image = request.json['image']
+    decodeImage(image, clApp.filename)
+    result = clApp.classifier.predict()
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    clApp = ClientApp()
+
+    app.run(host='0.0.0.0', port=8080) #for AWS
+
